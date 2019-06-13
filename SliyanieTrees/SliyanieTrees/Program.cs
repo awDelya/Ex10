2efaf3ce3183b7db0f76cc4cbf9472f57fb2fbe1
Овчинникова Аделина <deley@mail.ru>
2019-06-14 01:23:39 +0500

    Bugs fix

--- SliyanieTrees/SliyanieTrees/Program.cs
+++ SliyanieTrees/SliyanieTrees/Program.cs
@@ -9,24 +9,22 @@ namespace SliyanieTrees
 {
     class Program
     {
-        public static Random rnd = new Random();
-        public static int HowCreate = 0, size1, size2, i = 0;
-        public static Tree idTree1 = null, idTree2 = null, idTree = null;
-        public static Tree[] mas;
-        public static void Create()
+        private static Random rnd = new Random();
+        private static int HowCreate = 0, size1, size2, i = 0;
+        private static Tree idTree1 = null, idTree2 = null, idTree = null;
+        private static Tree[] mas;
+        private static void Create()
         {
-            Color.Print("\nВведите количество элементов для первого дерева: ", ConsoleColor.Yellow);
+            Color.Print("\n Введите количество элементов для первого дерева: ", ConsoleColor.Yellow);
             size1 = Number.Check(1, int.MaxValue);
-            Color.Print("\nВведите количество элементов для второго дерева: ", ConsoleColor.Yellow);
+            Color.Print("\n Введите количество элементов для второго дерева: ", ConsoleColor.Yellow);
             size2 = Number.Check(1, int.MaxValue);
             mas = new Tree[size1 + size2];
-            Color.Print("\nВыберите способ заполнения: \n\n ", ConsoleColor.Yellow);
-            Color.Print("1) Ручной ввод \n 2) Случайный \n Номер:  ");
-            HowCreate = Number.Check(1, 2);
+            HowCreate = Text.HowAdd();
             Console.Clear();
         }
 
-        static Tree IdealTree(int size, Tree p, int typeCreate)
+        private static Tree IdealTree(int size, Tree p, int typeCreate)
         {
             Tree r;
             int nl, nr;
@@ -46,7 +44,7 @@ namespace SliyanieTrees
             p = r;
             return p;
         }
-        static Tree IdealTree(int size, Tree p)
+        private static Tree IdealTree(int size, Tree p)
         {
             Tree r;
             int nl, nr;
@@ -66,7 +64,7 @@ namespace SliyanieTrees
             p = r;
             return p;
         }
-        static Tree MakeTree()
+        private static Tree MakeTree()
         {
             double x;
             if (HowCreate == 1)
@@ -84,34 +82,32 @@ namespace SliyanieTrees
                 return new Tree(x);
             }
         }
-        static void ShowTree(Tree p, int l)
+        private static void ShowTree(Tree p, int l)
         {
             if (p != null)
             {
                 ShowTree(p.left, l + 3);//переход к левому поддереву
                 for (int i = 0; i < l; i++) Console.Write(" ");
-                Color.Print(Convert.ToString(p.data) + "\n", ConsoleColor.Yellow);
+                Color.Print(Convert.ToString(p.data) + "\n", ConsoleColor.Cyan);
                 ShowTree(p.right, l + 3);//переход к правому поддереву
             }
         }
-        public static void Rezult()
+        private static void Rezult()
         {
             Console.Clear();
-            Color.Print("\nДерево 1:\n\n", ConsoleColor.Green);
+            Color.Print("\n Дерево 1:\n\n", ConsoleColor.Green);
             ShowTree(idTree1, 6);
-            Color.Print("\nДерево 2:\n\n", ConsoleColor.Green);
+            Color.Print("\n Дерево 2:\n\n", ConsoleColor.Green);
             ShowTree(idTree2, 6);
             i = 0;
             idTree = IdealTree(size1 + size2, idTree);
-            Color.Print("\nИтог:\n\n", ConsoleColor.Green);
+            Color.Print("\n Итог:\n\n", ConsoleColor.Green);
             ShowTree(idTree, 6);
         }
         static void Main()
         {
             Create();
-            Color.Print("\nДерево 1: ", ConsoleColor.Green);
             idTree1 = IdealTree(size1, idTree1, HowCreate);
-            Color.Print("\nДерево 2: ", ConsoleColor.Green);
             idTree2 = IdealTree(size2, idTree2, HowCreate);
             Rezult();
             Text.GoBackMenu();
