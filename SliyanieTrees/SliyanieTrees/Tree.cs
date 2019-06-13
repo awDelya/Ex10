9a9ffa446a84c10f2b96c40a6f7960ade509df45
Овчинникова Аделина <deley@mail.ru>
2019-06-13 23:16:44 +0500

    Add class Tree

---
+++ SliyanieTrees/SliyanieTrees/Tree.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SliyanieTrees
+{
+    class Tree
+    {
+        public double data;
+        public Tree left, right;
+        public Tree()
+        {
+            data = 0;
+            left = null;
+            right = null;
+        }
+        public Tree(double d)
+        {
+            data = d;
+            left = null;
+            right = null;
+        }
+        public override string ToString()
+        {
+            return data.ToString() + " ";
+        }
+    }
+}
